a19790560b9c8116a5b5f125b7d0c3d307ab48aa
Wiley Lau <wileylau@gmail.com>
2023-04-09 20:36:42 +0800

    ScoreSys: index: link to leaderboard

--- index.php
+++ index.php
@@ -66,14 +66,16 @@
     <div id="card-div"></div>
 
     <!-- Leaderboards -->
-    <div id="top-card">
-        <div style="background: #A5D61D;" id="icon-div">
-            <svg width="3.8vh" height="3.8vh" stroke-width="1.5" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" color="#E7E6FE">
-                <path d="M9 5h12M5 7V3L3.5 4.5M5.5 14h-2l1.905-2.963a.428.428 0 00.072-.323C5.42 10.456 5.216 10 4.5 10c-1 0-1 .889-1 .889s0 0 0 0v.222M4 19h.5a1 1 0 011 1v0a1 1 0 01-1 1h-1M3.5 17h2L4 19M9 12h12M9 19h12" stroke="#E7E6FE" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path>
-            </svg>
+    <a href="/admin/leaderboard.php">
+        <div id="top-card">
+            <div style="background: #A5D61D;" id="icon-div">
+                <svg width="3.8vh" height="3.8vh" stroke-width="1.5" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" color="#E7E6FE">
+                    <path d="M9 5h12M5 7V3L3.5 4.5M5.5 14h-2l1.905-2.963a.428.428 0 00.072-.323C5.42 10.456 5.216 10 4.5 10c-1 0-1 .889-1 .889s0 0 0 0v.222M4 19h.5a1 1 0 011 1v0a1 1 0 01-1 1h-1M3.5 17h2L4 19M9 12h12M9 19h12" stroke="#E7E6FE" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path>
+                </svg>
+            </div>
+            <p id="item-title">Leaderboards</p>
         </div>
-        <p id="item-title">Leaderboards</p>
-    </div>
+    </a>
 
     <!-- Individual Record Editor -->
     <div id="mid-card">
