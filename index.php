fe76d765a55368238a302f518364afce3eabb0e2
Wiley Lau <wileylau@gmail.com>
2023-04-11 11:53:46 +0800

    ScoreSys: index: drop individual record editor
    * Will be done directly in phpMyAdmin panel

--- index.php
+++ index.php
@@ -79,20 +79,10 @@
         </div>
     </a>
 
-    <!-- Individual Record Editor -->
-    <div id="mid-card">
-        <div style="background: #AC49F5;" id="icon-div">
-            <svg width="3.8vh" height="3.8vh" stroke-width="1.5" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" color="#E7E6FE">
-                <path d="M14.363 5.652l1.48-1.48a2 2 0 012.829 0l1.414 1.414a2 2 0 010 2.828l-1.48 1.48m-4.243-4.242l-9.616 9.615a2 2 0 00-.578 1.238l-.242 2.74a1 1 0 001.084 1.085l2.74-.242a2 2 0 001.24-.578l9.615-9.616m-4.243-4.242l4.243 4.242" stroke="#E7E6FE" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path>
-            </svg>
-        </div>
-        <p id="item-title">Edit Individual Records</p>
-    </div>
-
     <!-- Participation Check -->
     <a href="/admin/event-stats.php">
         <div id="mid-card">
-            <div style="background: #E64566;" id="icon-div">
+            <div style="background: #AC49F5;" id="icon-div">
                 <svg width="3.8vh" height="3.8vh" stroke-width="1.5" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" color="#E7E6FE">
                     <path d="M17 17l4 4M3 11a8 8 0 1016 0 8 8 0 00-16 0z" stroke="#E7E6FE" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path>
                 </svg>
@@ -104,7 +94,7 @@
     <!-- Event Stat -->
     <a href="/admin/statistics.php">
         <div id="bottom-card">
-            <div style="background: #E84026;" id="icon-div">
+            <div style="background: #E64566;" id="icon-div">
                 <svg width="3.8vh" height="3.8vh" stroke-width="1.5" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" color="#E7E6FE">
                     <path d="M16 20v-8m0 0l3 3m-3-3l-3 3M4 14l8-8 3 3 5-5" stroke="#E7E6FE" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path>
                 </svg>
@@ -118,7 +108,7 @@
     <!-- Lock event -->
     <a href="/admin/event-manager.php">
         <div id="top-card">
-            <div style="background: #ED6F21;" id="icon-div">
+            <div style="background: #E84026;" id="icon-div">
                 <svg width="3.8vh" height="3.8vh" stroke-width="1.5" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" color="#E7E6FE">
                     <path d="M16 12h1.4a.6.6 0 01.6.6v6.8a.6.6 0 01-.6.6H6.6a.6.6 0 01-.6-.6v-6.8a.6.6 0 01.6-.6H8m8 0V8c0-1.333-.8-4-4-4S8 6.667 8 8v4m8 0H8" stroke="#E7E6FE" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path>
                 </svg>
@@ -130,7 +120,7 @@
     <!-- phpmyadmin panel -->
     <?php echo "<a href='http://" . $_SERVER['SERVER_NAME'] . "/phpmyadmin'>"; ?>
     <div id="mid-card">
-        <div style="background: #F9A01E;" id="icon-div">
+        <div style="background: #ED6F21;" id="icon-div">
             <svg width="3.8vh" height="3.8vh" stroke-width="1.5" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" color="#E7E6FE">
                 <path d="M4 6v6s0 3 7 3 7-3 7-3V6" stroke="#E7E6FE" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path>
                 <path d="M11 3c7 0 7 3 7 3s0 3-7 3-7-3-7-3 0-3 7-3zM11 21c-7 0-7-3-7-3v-6M19 21a2 2 0 100-4 2 2 0 000 4z" stroke="#E7E6FE" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path>
@@ -144,7 +134,7 @@
     <!-- source -->
     <a href="https://www.github.com/rain2wood/ScoreSys">
         <div id="bottom-card">
-            <div style="background: #F7CE00;" id="icon-div">
+            <div style="background: #F9A01E;" id="icon-div">
                 <svg width="3.8vh" height="3.8vh" stroke-width="1.5" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" color="#E7E6FE">
                     <path d="M13.5 6L10 18.5M6.5 8.5L3 12l3.5 3.5M17.5 8.5L21 12l-3.5 3.5" stroke="#E7E6FE" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path>
                 </svg>
